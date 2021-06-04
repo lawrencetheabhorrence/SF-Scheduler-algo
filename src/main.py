35dd13f3c27371bbd6a1302361ce0c5761fc83ad
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-06-04 11:29:20 +0800

    bigger sf data

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ def __main__():
     pop_size = 50
     threshold = 0.05
     mutation_prop = 0.1
-    game_src = "algo/test/data/test_game_data.csv"
-    sf_src = "algo/test/data/test_sf_data.csv"
+    game_src = "algo/test/data/big_game_data.csv"
+    sf_src = "algo/test/data/big_sf_data.csv"
     t_start = time.perf_counter()
     print(genetic_algo(pop_size, threshold, mutation_prop, game_src, sf_src))
     t_end = time.perf_counter()
