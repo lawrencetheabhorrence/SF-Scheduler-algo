1a0f246c26169dd957d24363f6ac7040efa52d36
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-11-03 13:59:35 +0800

    added conversion of POST data to csv

--- src/webclient/webclient/settings.py
+++ src/webclient/webclient/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -50,6 +52,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'webclient.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5000',
+]
 
 TEMPLATES = [
     {
