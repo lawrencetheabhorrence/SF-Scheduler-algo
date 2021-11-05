257e2fece5a070900e0981a8a8a6547f92074eda
RakiRarufu <armayores@evc.pshs.edu.ph>
2021-11-05 12:23:40 +0800

    regenerate and hide secret key

--- src/webclient/webclient/settings.py
+++ src/webclient/webclient/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%iix7k^o9@rc3$p!73_#p^ug=)-3bb*)7_9pvpo8=c#-m%3m9z'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
