d41fe3b7eef8a9bc18958c559eb1dc17aff20c52
RakiRarufu <armayores@evc.pshs.edu.ph>
2022-03-05 20:48:44 +0800

    fix datetime import error

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 import os
 import sys
-import datetime.datetime as dt
+import datetime as dt
+from datetime import datetime
 from ga.GeneticAlgo import GeneticAlgo
 from ga.data.reader import read_game_data, read_sf_data
 from ga.data.output import bits_to_sched
