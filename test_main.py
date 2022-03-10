98ee90e7d283cd81bf72e1a7ccc48fa3ab64c496
RakiRarufu <armayores@evc.pshs.edu.ph>
2022-03-10 11:53:33 +0800

    make generations variable in test main, add result writing functionality

--- test_main.py
+++ test_main.py
@@ -1,24 +1,29 @@
 """ this is mostly for testing timing and fitness """
 import time
 import os
+import sys
 from ga.GeneticAlgo import GeneticAlgo
-import datetime
+from ga.data.reader import read_game_data, read_sf_data
+from ga.data.output import bits_to_sched
+from datetime import datetime as dt
 
 root = os.path.dirname(os.path.abspath(__file__))
 big_folder = '/data/model'
 tiny_folder = '/ga/data/test'
+date = dt.today().strftime("%Y-%m-%d-%H:%M:%S")
+gen = int(sys.argv[1])
 
 # test parameters
 ga_params = {
     'selection_method': 'rank',
     'crossover_method': 'uniform',
     'mutation_method': 'bit_flip',
-    'threshold': 10,
+    'threshold': gen,
     'pop_size': 50,
     'mutation_rate': 0.1,
     'game_src': root + big_folder + '/big_game_data_og.csv',
     'sf_src': root + big_folder + '/big_sf_data_og.csv',
-    'fitness_src': root + big_folder + f"{datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S')}_fitness.csv",
+    'fitness_src': root + big_folder + f"_{gen}_{date}_fitness.csv",
     'crossover_params': {'children': 2, 'n_breaks': 5}
 }
 
@@ -40,7 +45,14 @@ def __main__():
     print(best)
     t_end = time.perf_counter()
     print(f"Time in seconds: {t_end-t_start:0.4f}")
-    with open(f"{datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S')}.txt", "w") as f:
+    with open(f"{gen}_{date}.txt", "w") as f:
         f.write(f"Time in seconds: {t_end-t_start:0.4f}")
-
+    sf_data = read_sf_data(ga_params['sf_src'])
+    game_data = read_game_data(ga_params['game_src'], sf_data['teams'])
+    df = bits_to_sched(best, sf_data, game_data)
+    for i, day in enumerate(df):
+        day.to_html(root + big_folder + f'_{gen}_{date}_result_{i}.html', escape=False)
+        day.to_csv(root + big_folder + f'_{gen}_{date}_result_{i}.csv')
+        global days
+        days = i + 1
 __main__()
